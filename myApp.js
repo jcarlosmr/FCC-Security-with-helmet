f91ed1c94a8ee75fd6c0b90218cf42ce55bd50e3
Juan Carlos Mendoza R <jcarlosmr@gmail.com>
2022-06-15 01:13:50 -0500

    frameguard

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
+helmet.frameguard({ action: "deny" });
 app.use(helmet.hidePoweredBy());
 
 module.exports = app;
