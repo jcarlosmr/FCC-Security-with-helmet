ba05eaf984bf9fcdefdf47f7a691a8a4b198bd2e
Juan Carlos Mendoza R <jcarlosmr@gmail.com>
2022-06-15 00:33:24 -0500

    app.use(helmet.hidePoweredBy());

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 const helmet = require("helmet");
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
-app.use(helmet.hidePoweredBy());
 app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
